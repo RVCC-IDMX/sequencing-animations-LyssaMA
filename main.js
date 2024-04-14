0c00299a390d1091c4a0b07a498cf805405a1145
Alyssa <alyssam.anderson@yahoo.com>
2024-04-14 13:38:57 -0700

    promise chain

--- main.js
+++ main.js
@@ -18,6 +18,7 @@ const alice3 = document.querySelector("#alice3");
 // console.log(animation1);
 */
 
+/*
 //callback hell
 const aliceTumbling = [
   { transform: 'rotate(0) scale(1)' },
@@ -56,5 +57,49 @@ startAnimation1(() => {
     });
   });
 });
+*/
 
 
+// promise chain
+const aliceTumbling = [
+  { transform: 'rotate(0) scale(1)' },
+  { transform: 'rotate(360deg) scale(0)' }
+];
+
+const aliceTiming = {
+  duration: 2000,
+  iterations: 1,
+  fill: 'forwards'
+};
+
+const alice1 = document.querySelector("#alice1");
+const alice2 = document.querySelector("#alice2");
+const alice3 = document.querySelector("#alice3");
+
+function startAnimation1() {
+  return new Promise((resolve) => {
+    const animation1 = alice1.animate(aliceTumbling, aliceTiming);
+    animation1.onfinish = resolve;
+  });
+}
+
+const startAnimation2 = () => {
+  return new Promise((resolve) => {
+    const animation2 = alice2.animate(aliceTumbling, aliceTiming);
+    animation2.onfinish = resolve;
+  });
+};
+
+const startAnimation3 = () => {
+  return new Promise((resolve) => {
+    const animation3 = alice3.animate(aliceTumbling, aliceTiming);
+    animation3.onfinish = resolve;
+  });
+};
+
+startAnimation1()
+  .then(() => startAnimation2())
+  .then(() => startAnimation3())
+  .then(() => {
+    console.log("All animations complete");
+  });
